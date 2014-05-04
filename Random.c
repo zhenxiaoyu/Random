fc08739114d886af718c44b4e10347a0911d655d
shaozhenyu <363883861@qq.com>
2014-05-04 22:13:34 +0800

    Random

---
+++ Random.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Random.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年05月04日 22时06分26秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+main()
+{
+	int group[10][4] = {0};
+	int i,j;
+	srand(time(NULL));
+	for(i = 1;i <= 39;i++)
+	{
+		j = rand() % 40; 
+		while(*(group[0] + j % 40) != 0)
+			j++;
+		*(group[0] + j % 40) = i;
+	}
+	for(i=0;i<10;i++)
+	{
+		printf("第%d组：",i+1);
+		for(j=0;j<4;j++)
+			printf("%d ",group[i][j]);
+		printf("\n");
+	}
+}
